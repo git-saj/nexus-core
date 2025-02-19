22dc3bf5aac787023578092397c42ea4ce13cbdb
git-saj <git-saj@sajbox.com>
2025-02-19 23:24:57 +0000

    added users

--- nix/hosts/common/global/user.nix
+++ nix/hosts/common/global/user.nix
@@ -1,11 +1,12 @@
 { pkgs, ... }:
 {
-  users.user.s0010054j = {
+  users.users.s0010054j = {
     isNormalUser = true;
     extraGroups = [ "wheel" "docker" ];
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIf6QboK9ZCFTn7fmSywFWeg9gY620qvqG7g1xXKjoz1"
     ];
+  };
   nix.settings.allowed-users = [ "s0010054j" ];
 }
