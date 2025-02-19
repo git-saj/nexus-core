1c250da50b5bacf7afccb773d162b84cf9803259
git-saj <git-saj@sajbox.com>
2025-02-19 23:20:38 +0000

    added common user

--- nix/hosts/common/global/user.nix
+++ nix/hosts/common/global/user.nix
@@ -6,6 +6,6 @@
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIf6QboK9ZCFTn7fmSywFWeg9gY620qvqG7g1xXKjoz1"
-  };
+    ];
   nix.settings.allowed-users = [ "s0010054j" ];
 }
