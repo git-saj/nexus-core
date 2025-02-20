e6e8ae56f367dbcb194496115f9bcfb226f09989
git-saj <git-saj@sajbox.com>
2025-02-20 00:50:46 +0000

    moved to root user

--- nix/hosts/common/global/user.nix
+++ nix/hosts/common/global/user.nix
@@ -1,8 +1,6 @@
 { pkgs, ... }:
 {
   users.users.root = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "docker" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIf6QboK9ZCFTn7fmSywFWeg9gY620qvqG7g1xXKjoz1"
     ];
