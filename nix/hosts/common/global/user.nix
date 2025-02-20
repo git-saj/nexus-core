15d85553440ebb7f5675dcdb5c0d340c952df3ea
git-saj <git-saj@sajbox.com>
2025-02-20 00:02:06 +0000

    added boot

--- nix/hosts/common/global/user.nix
+++ nix/hosts/common/global/user.nix
@@ -8,5 +8,5 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIf6QboK9ZCFTn7fmSywFWeg9gY620qvqG7g1xXKjoz1"
     ];
   };
-  nix.settings.allowed-users = [ "s0010054j" ];
+  nix.settings.allowed-users = [ "root" "@wheel" ];
 }
