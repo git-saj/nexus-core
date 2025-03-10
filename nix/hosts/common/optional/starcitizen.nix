17274c668a72a5c1749b538f048edb0fb29c6f2c
git-saj <git-saj@sajbox.com>
2025-03-10 14:14:50 +0000

    added monitor config for sc

--- nix/hosts/common/optional/starcitizen.nix
+++ nix/hosts/common/optional/starcitizen.nix
@@ -4,6 +4,20 @@
     (inputs.nix-gaming.packages.${pkgs.system}.star-citizen.override {
       disableEac = false;
       useUmu = true;
+      gamescope = pkgs.gamescope.overrideAttrs (_: {
+        NIX_CFLAGS_COMPILE = [ "-fno-fast-math" ];
+      });
+      gameScopeEnable = true;
+      gameScopeArgs = [
+        "--fullscreen"
+        "--expose-wayland"
+        "--force-grab-cursor"
+        "--force-windows-fullscreen"
+        "--prefer-output DP-1"
+        "--output-width 2560"
+        "--output-height 1440"
+        "--framerate-limit 144"
+      ];
       preCommands = ''
         export __GL_SHADER_DISK_CACHE=true
         export __GL_SHADER_DISK_CACHE_PATH="$WINEPREFIX/nvidiacache"
