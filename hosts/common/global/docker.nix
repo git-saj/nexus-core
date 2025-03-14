3b132facda8e5f1b514100d2c9cb090f0f14a0c3
git-saj <git-saj@sajbox.com>
2025-03-14 01:21:38 +0000

    Configure Docker logs with size and rotation limits

--- hosts/common/global/docker.nix
+++ hosts/common/global/docker.nix
@@ -2,5 +2,7 @@
   virtualisation.docker = {
     enable = true;
     package = pkgs.docker_26;
+    logDriver = "json-file";
+    extraOptions = "--log-opt max-size=10m --log-opt max-file=3";
   };
 }
