63b81fb11bd53b6220bcad197e5da79db37718af
git-saj <git-saj@sajbox.com>
2025-08-23 23:17:46 +0100

    added nvidia devide plugin

--- hosts/common/global/docker.nix
+++ hosts/common/global/docker.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   virtualisation.docker = {
     enable = true;
+    enableNvidia = true;
     package = pkgs.docker_26;
     logDriver = "json-file";
     extraOptions = "--log-opt max-size=10m --log-opt max-file=3";
